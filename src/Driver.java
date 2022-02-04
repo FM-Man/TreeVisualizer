e7d9a93cf3a18f3e23144f802f2d71a07c381ad4
fahim-morshed-1102 <bsse1102@iit.du.ac.bd>
2022-02-04 08:50:42 +0600

    print lineage

--- src/Driver.java
+++ src/Driver.java
@@ -4,38 +4,22 @@ public class Driver {
     public static void main(String[] args) {
         ArrayList<Node> al = new ArrayList<>();
         Node a = new Node("a");
-        Node b = new Node("b");
-        Node c = new Node("c");
-        c.addChild(a);
-        c.addChild(b);
-        Node d = new Node("d");
-        Node e = new Node("e");
-        Node f = new Node("f");
-        f.addChild(d);
-        f.addChild(e);
-        Node g = new Node("g");
-        g.addChild(c);
-        g.addChild(f);
+        Node b = new Node(a,"b");
+        Node c = new Node(a,"c");
+        Node d = new Node(b,"d");
+        Node e = new Node(b,"e");
+        Node f = new Node(c,"f");
+        Node g = new Node(c,"g");
+        Node h = new Node(d,"h");
+        Node i = new Node(d,"i");
+        Node j = new Node(e,"j");
+        Node k = new Node(e,"k");
+        Node l = new Node(f,"l");
+        Node m = new Node(f,"m");
+        Node n = new Node(g,"n");
+        Node o = new Node(g,"o");
 
-        Node h = new Node("h");
-        Node i = new Node("i");
-        Node j = new Node("j");
-        j.addChild(h);
-        j.addChild(i);
-        Node k = new Node("k");
-        Node l = new Node("l");
-        Node m = new Node("m");
-        m.addChild(k);
-        m.addChild(l);
-        Node n = new Node("n");
-        n.addChild(j);
-        n.addChild(m);
-
-        Node o = new Node("o");
-        o.addChild(g);
-        o.addChild(n);
-
-        Tree tree = new Tree(o);
+        Tree tree = new Tree(a);
         tree.traverse(tree.root);
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -4,13 +4,27 @@ public class Tree {
         this.root = root;
     }
 
+    private String printLineage(Node start){
+        String s ="";
+        s+=start.name;
+        Node i = start;
+        while(i.parent != null){
+            s+="->"+i.parent.name;
+            i = i.parent;
+        }
+        return s;
+    }
+
     public void traverse(Node start){
         start.isVisited = true;
-        System.out.print(start.name+"->");
+
+        System.out.println("("+start.name+")");//insert name here
+
         for (Node child : start.children) {
-            if(!child.isVisited) traverse(child);
+            if(!child.isVisited) traverse(child);//need to print the horizontal line here
         }
 
+        System.out.println(printLineage(start));//increase width here
     }
 }
 //o->g->c->a->b->f->d->e->n->j->h->i->m->k->l->
